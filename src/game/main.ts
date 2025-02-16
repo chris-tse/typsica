496637119b7a37034354fe51787861e6cc474433
Chris Tse <chris@chris-tse.com>
2025-02-16 03:57:57 -0600

    don't debug physics on prod

--- src/game/main.ts
+++ src/game/main.ts
@@ -21,7 +21,7 @@ const config: Phaser.Types.Core.GameConfig = {
 		default: 'arcade',
 		arcade: {
 			gravity: { x: 0, y: 0 },
-			debug: true,
+			debug: process.env.NODE_ENV !== 'production',
 		},
 	},
 }
